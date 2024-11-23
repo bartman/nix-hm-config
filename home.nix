cd692bf083eee725a1d0ca2d700f6191745ae8da
Bart Trojanowski <bart@jukie.neet>
2024-11-23 17:05:50 -0500

    zsh spruced up

--- home.nix
+++ home.nix
@@ -1,99 +1,58 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "bart";
   home.homeDirectory = "/home/bart";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [
+    coreutils
+    neovim
+    gh
+    fd
+    ripgrep
+    tree
+    tmux
 
-    pkgs.coreutils
-    pkgs.neovim
-    pkgs.gh
-    pkgs.fd
-    pkgs.ripgrep
-    pkgs.tree
-    pkgs.tmux
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
+    # (writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # ----------------------------------------------------------------------
+  # dot files
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
     ".config/tmux".source = dot/tmux;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/bart/etc/profile.d/hm-session-vars.sh
-  #
+  # ----------------------------------------------------------------------
+  # environment
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
+    # this could aso be a file, like ~/.local/profile.sh
   };
 
   programs.man.generateCaches = true;
+
+  # ----------------------------------------------------------------------
+  # git
   programs.git = {
     enable = true;
     userName = "Bart Trojanowski";
     userEmail = "bart@jukie.neet";
   };
 
+  # ----------------------------------------------------------------------
+  # shell config
   programs.bash = {
     enable = true;
     historySize = 10000;
     historyFileSize = 100000;
     shellAliases = {
+      vim = "nvim";
       ls = "ls --color=auto";
     };
     sessionVariables = {
@@ -104,20 +63,55 @@
     defaultKeymap = "viins";
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    enableCompletion = true;
     history.size = 100000;
     shellAliases = {
+      vim = "nvim";
       ls = "ls --color=auto";
     };
     sessionVariables = {
     };
   };
-  programs.dircolors = {
-    enable = true;
+
+  # ----------------------------------------------------------------------
+  # shell extensions
+  programs.atuin.enable = false;             # shell history
+  programs.autojump.enable = false;          # cd-replacement
+  programs.broot.enable = false;             # interactive fuzzy tree
+  programs.carapace.enable = true;           # command line completion generator
+  programs.dircolors.enable = true;          # LS_COLORS
+  programs.eww.enable = false;               # widgets
+  programs.eza.enable = true;                # ls replacement
+  programs.fzf.enable = true;                # fuzzy finder (ctrl-R)
+  programs.granted.enable = false;           # cloud stuff
+  programs.hstr.enable = false;              # history search
+  programs.keychain.enable = true;           # ssh/gpg key manager
+  programs.kitty.shellIntegration = {
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
-  programs.starship.enable = true;
-  programs.fzf.enable = true;
+  programs.mcfly.enable = false;             # history manager (fzf is better)
+  programs.mise.enable = false;              # manages dev environments
+  programs.navi.enable = true;               # cheat sheet for cli commands (ctrl-G)
+  programs.nix-index.enable = true;          # fast nixpkgs index
+  programs.oh-my-posh.enable = false;        # prompt theme engine
+  programs.opam.enable = false;              # OCaml packages
+  programs.pazi.enable = false;              # autojump helper
+  programs.pyenv.enable = false;             # python package version manager
+  programs.rbenv.enable = false;             # ruby package version manager
+  programs.scmpuff.enable = false;           # git cli shortcuts
+  programs.skim.enable = false;              # fuzzy finder (ctrl-R)
+  programs.starship.enable = true;           # prompt theme engine
+  programs.thefuck.enable = false;           # correct failed command
+  programs.watson.enable = false;            # cli time tracker
+  programs.wezterm.enable = false;           # terminal
+  programs.yazi.enable = false;              # terminal file manager
+  programs.zellij.enable = false;            # tmux successor
+  programs.z-lua.enable = false;             # cd-replacement
+  programs.zoxide.enable = true;             # cd-replacement
+
+  # services
+  #services.gpg-agent.enable = false;
 
   #programs.tmux = {
   #  enable = true;
