174e18076f7241f5f6a00faa0d01470f08bb3b5c
Bart Trojanowski <bart@jukie.neet>
2024-11-23 19:29:40 -0500

    cleanup programs

--- home.nix
+++ home.nix
@@ -75,32 +75,32 @@
 
   # ----------------------------------------------------------------------
   # shell extensions
-  programs.atuin.enable = false;             # shell history
-  programs.autojump.enable = false;          # cd-replacement
-  programs.broot.enable = false;             # interactive fuzzy tree
-  programs.carapace.enable = true;           # command line completion generator
-  programs.dircolors.enable = true;          # LS_COLORS
-  programs.eww.enable = false;               # widgets
-  programs.eza.enable = true;                # ls replacement
-  programs.fzf.enable = true;                # fuzzy finder (ctrl-R)
-  programs.granted.enable = false;           # cloud stuff
-  programs.hstr.enable = false;              # history search
-  programs.keychain.enable = true;           # ssh/gpg key manager
+  #programs.atuin.enable = true;             	# shell history
+  #programs.autojump.enable = true;          	# cd-replacement
+  #programs.broot.enable = true;             	# interactive fuzzy tree
+  programs.carapace.enable = true;           	# command line completion generator
+  programs.dircolors.enable = true;          	# LS_COLORS
+  #programs.eww.enable = true;               	# widgets
+  programs.eza.enable = true;                	# ls replacement
+  programs.fzf.enable = true;                	# fuzzy finder (ctrl-R)
+  #programs.granted.enable = true;           	# cloud stuff
+  #programs.hstr.enable = true;              	# history search
+  programs.keychain.enable = true;           	# ssh/gpg key manager
   programs.kitty.shellIntegration = {
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
-  programs.mcfly.enable = false;             # history manager (fzf is better)
-  programs.mise.enable = false;              # manages dev environments
-  programs.navi.enable = true;               # cheat sheet for cli commands (ctrl-G)
-  programs.nix-index.enable = true;          # fast nixpkgs index
-  programs.oh-my-posh.enable = false;        # prompt theme engine
-  programs.opam.enable = false;              # OCaml packages
-  programs.pazi.enable = false;              # autojump helper
-  programs.pyenv.enable = false;             # python package version manager
-  programs.rbenv.enable = false;             # ruby package version manager
-  programs.scmpuff.enable = false;           # git cli shortcuts
-  programs.skim.enable = false;              # fuzzy finder (ctrl-R)
+  #programs.mcfly.enable = true;             	# history manager (fzf is better)
+  #programs.mise.enable = true;              	# manages dev environments
+  programs.navi.enable = true;               	# cheat sheet for cli commands (ctrl-G)
+  programs.nix-index.enable = true;          	# fast nixpkgs index
+  #programs.oh-my-posh.enable = true;        	# prompt theme engine
+  #programs.opam.enable = true;              	# OCaml packages
+  #programs.pazi.enable = true;              	# autojump helper
+  #programs.pyenv.enable = true;             	# python package version manager
+  #programs.rbenv.enable = true;             	# ruby package version manager
+  #programs.scmpuff.enable = true;           	# git cli shortcuts
+  #programs.skim.enable = true;              	# fuzzy finder (ctrl-R)
   programs.starship = {                      # prompt theme engine
     enable = true;
     settings = {
@@ -115,13 +115,13 @@
       };
     };
   };
-  programs.thefuck.enable = false;           # correct failed command
-  programs.watson.enable = false;            # cli time tracker
-  programs.wezterm.enable = false;           # terminal
-  programs.yazi.enable = false;              # terminal file manager
-  programs.zellij.enable = false;            # tmux successor
-  programs.z-lua.enable = false;             # cd-replacement
-  programs.zoxide.enable = true;             # cd-replacement
+  #programs.thefuck.enable = true;           	# correct failed command
+  #programs.watson.enable = true;            	# cli time tracker
+  #programs.wezterm.enable = true;           	# terminal
+  #programs.yazi.enable = true;              	# terminal file manager
+  #programs.zellij.enable = true;            	# tmux successor
+  #programs.z-lua.enable = true;             	# cd-replacement
+  programs.zoxide.enable = true;             	# cd-replacement
 
   # services
   #services.gpg-agent.enable = false;
