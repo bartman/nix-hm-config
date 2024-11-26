d1d6b8690d75ca8d85ab22ee00f621a23990f80f
Bart Trojanowski <bart@jukie.neet>
2024-11-25 21:28:48 -0500

    fix the Nix switch to zsh/bash

--- switch-to-nix-bash.nix
+++ switch-to-nix-bash.nix
@@ -2,14 +2,20 @@
 # it will switch to the nix shell
 ''
   # when interactive, remotely connected, and not in tmux session
-  if [[ $- == *i* && -n "$SSH_CONNECTION" && -z "$TMUX" ]] ; then
+  if [[ -n "$BASH_VERSION" && $- == *i* && -n "$SSH_CONNECTION" && -z "$TMUX" ]] ; then
+    #echo "consider switching to Nix bash"
     # if we have an executable nix bash, switch to that instead
+    _run_bash="$(readlink /proc/$$/exe)"
     _nix_bash="$HOME/.nix-profile/bin/bash"
-    if [[ -x $_nix_bash && ! $nix_bash -ef $SHELL ]] ; then
-      echo switching $SHELL to Nix $_nix_zsh
+    if [[ -x $_nix_bash && ! $nix_bash -ef $_run_bash ]] ; then
+      #echo switching $_run_bash to Nix $_nix_zsh
       export SHELL="$_nix_bash"
       exec "$SHELL" -i
     fi
-    unset _nix_bash
+    if [[ -d $HOME/.nix-profile/bin ]] && ! which home-manager &>/dev/null 2>&1 ; then
+      # sometimes Nix/bin is not in the path, but why?
+      export PATH="$HOME/.nix-profile/bin:$PATH"
+    fi
+    unset _nix_bash _run_bash
   fi
 ''
--- switch-to-nix-zsh.nix
+++ switch-to-nix-zsh.nix
@@ -2,17 +2,25 @@
 # it will switch to the nix shell
 ''
   # when interactive, remotely connected, and not in tmux session
-  if [[ $- == *i* && -n "$SSH_CONNECTION" && -z "$TMUX" ]] ; then
+  if [[ -n "$ZSH_VERSION" && $- == *i* && -n "$SSH_CONNECTION" && -z "$TMUX" ]] ; then
+    #echo "consider switching to Nix zsh"
     # if we have an executable nix zsh, switch to that instead
-    _nix_zsh="$HOME/.nix-profile/bin/zsh"
+    _nix_zsh="$(readlink -f "$HOME/.nix-profile/bin/zsh")"
     _nix_stat=$(stat -c '%i' $_nix_zsh)
-    _zsh_stat=$(stat -c '%i' $SHELL)
-    if [[ -x $_nix_zsh && -n "$_nix_stat" && $_nix_stat != $_zsh_stat ]] ; then
-      echo switching $SHELL to Nix $_nix_zsh
-      export SHELL="$_nix_zsh"
+    _run_zsh="$(readlink -f /proc/$$/exe)"
+    _run_stat=$(stat -c '%i' $_run_zsh)
+    if [[ -x $_nix_zsh && -n "$_nix_stat" && $_nix_stat != $_run_stat ]] ; then
+      #echo switching $_run_zsh to Nix $_nix_zsh
+      #export SHELL="$_nix_zsh"
       unset __HM_SESS_VARS_SOURCED __HM_ZSH_SESS_VARS_SOURCED
-      exec "$SHELL" -i
+      exec "$_nix_zsh" -i -l
     fi
-    unset _nix_zsh _nix_stat _zsh_stat
+    if [[ -d $HOME/.nix-profile/bin ]] ; then
+      # sometimes Nix/bin is not in the path, but why?
+      path=( "$HOME/.nix-profile/bin" $path )
+      typeset -U path
+    fi
+    #echo "path=( $path )"
+    unset _nix_zsh _nix_stat _run_zsh _run_stat
   fi
 ''
